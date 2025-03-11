1c53767fb57ba46a19e8354d9346d374e087550b
Alejandro <knight.moro@gmail.com>
2025-03-11 19:26:23 +0100

    agenda json a medias

--- agenda_json.py
+++ agenda_json.py
@@ -23,6 +23,39 @@ class Agenda():
                 
         else:
             print("La agenda está vacía.")
+
+    def introducir_contacto(self):
+        while True:
+            contacto = input("Introduzca el nombre del contacto: ")
+            nom_contacto = contacto.upper()
+            if nom_contacto not in self.contactos:
+                num_contacto = input("Introduzca el número del contacto: ")
+                email = input("Introduzca su E-mail: ")
+                self.contactos[nom_contacto] = {"Telefono": num_contacto, "Email": email} 
+                self.guardar_contactos()
+                break
+            else:
+                print("Este nombre ya figura en tu lista de contactos, por favor elige otro nombre.")
+                
+    def guardar_contactos(self): 
+        with open(self.archivo, "w") as f:
+            json.dump(self.contactos, f) 
+
+    def menu(self):
+        while True:
+            print(f"\nAgenda de teléfonos marca ACME \nSeleccione una opción: \nIntroduzca 1 para introducir un contacto. \nIntroduzca 2 para eliminar un contacto. \nIntroduzca 3 para buscar un contacto.\nIntroduzca 4 para ver toda la agenda.")
+            opcion=input(f"Introduzca cualquier otro valor para salir de la agenda.\n")
+            if opcion == "1":
+                contacto.introducir_contacto()
+            elif opcion == "2":
+                contacto.eliminar_contacto()
+            elif opcion == "3":
+                contacto.buscar_contacto()
+            elif opcion == "4":
+                contacto.mostrar_agenda()
+            else:
+                print("gracias por usar la agenda de teléfonos marca ACME")
+                break 
    
 contacto=Agenda()
-contacto.mostrar_agenda()
+contacto.menu()

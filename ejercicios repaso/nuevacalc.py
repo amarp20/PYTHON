613826e9a26b7320e24aa304ab7bfde0717cdd55
Alejandro <knight.moro@gmail.com>
2025-03-21 16:23:45 +0100

    error

--- ejercicios repaso/nuevacalc.py
+++ ejercicios repaso/nuevacalc.py
@@ -5,14 +5,14 @@ class Calculadora():
         self.numero2 = numero2
             
     def sumar(self):
-        print(f"El resultado de {self.numero1} + {self.numero2} es: {self.numero1 + self.numero2} ")
+        return self.numero1 + self.numero2 
         
     def restar(self):
-        print(f"El resultado de {self.numero1} - {self.numero2} es: {self.numero1 - self.numero2} ")
+        return self.numero1 - self.numero2
         
     def multiplicar(self):
-        print(f"El resultado de {self.numero1} X {self.numero2} es: {self.numero1 * self.numero2} ")
+        return self.numero1 * self.numero2
         
     def dividir(self):
-        print(f"El resultado de {self.numero1} / {self.numero2} es: {self.numero1 / self.numero2} ")
+        return self.numero1 / self.numero2
 
